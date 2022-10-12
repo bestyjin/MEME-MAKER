a29b92994cf5e863a207246284ebb81e914c9779
bestyjin <bestyjin0604@gmail.com>
2022-10-12 17:09:18 +0900

    JS : draw a man

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(200,200,50,150);
-ctx.fillRect(400,200,50,150);
-ctx.lineWidth = 2;
-ctx.fillRect(300,250,50,100);
-ctx.moveTo(200,200);
-ctx.lineTo(325,100);
-ctx.lineTo(450,200);
+ctx.fillRect(200,200,20,100);
+ctx.fillRect(300,200,20,100);
+ctx.fillRect(230,200,60,150);
+ctx.fill();
+
+ctx.arc(260,150,40,0,2*Math.PI);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "white";
+ctx.arc(240,150,5,Math.PI,2*Math.PI);
+ctx.arc(280,150,5,Math.PI,2*Math.PI);
 ctx.fill();
