1e3a777a2922fb753c53d32d21254e8837157e11
bestyjin <bestyjin0604@gmail.com>
2022-10-14 16:20:37 +0900

    JS : click event - draw line

--- app.js
+++ app.js
@@ -3,16 +3,10 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(200,200,20,100);
-ctx.fillRect(300,200,20,100);
-ctx.fillRect(230,200,60,150);
-ctx.fill();
+ctx.moveTo(0,0)
 
-ctx.arc(260,150,40,0,2*Math.PI);
-ctx.fill();
-
-ctx.beginPath();
-ctx.fillStyle = "white";
-ctx.arc(240,150,5,Math.PI,2*Math.PI);
-ctx.arc(280,150,5,Math.PI,2*Math.PI);
-ctx.fill();
+function onClick(event) {
+    ctx.lineTo(event.offsetX,event.offsetY);
+    ctx.stroke();
+}
+canvas.addEventListener("click", onClick);
