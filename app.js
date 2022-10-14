dd2e0656337cef7020665307b977bbbc4a1261fc
bestyjin <bestyjin0604@gmail.com>
2022-10-14 16:22:52 +0900

    JS : event mousemove - draw colorful lines

--- app.js
+++ app.js
@@ -3,10 +3,22 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.moveTo(0,0)
+const colors = [
+    "#ff3838",
+    "#ffb8b8",
+    "#c56cf0",
+    "#ff9f1a",
+    "#fff200",
+    "#32ff7e",
+    "#7efff5",
+    ]
+
 
 function onClick(event) {
+    ctx.beginPath();
+    ctx.moveTo(0,0)
+    ctx.strokeStyle = colors[Math.floor(Math.random()*colors.length)];
     ctx.lineTo(event.offsetX,event.offsetY);
     ctx.stroke();
 }
-canvas.addEventListener("click", onClick);
+canvas.addEventListener("mousemove", onClick);
