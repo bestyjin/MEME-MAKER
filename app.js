8c7c8262be09615524cb308f2abc9a4d667ff5d9
bestyjin <bestyjin0604@gmail.com>
2022-10-14 16:42:19 +0900

    JS : drawing

--- app.js
+++ app.js
@@ -3,27 +3,27 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-const colors = [
-    "#ff3838",
-    "#ffb8b8",
-    "#c56cf0",
-    "#ff9f1a",
-    "#fff200",
-    "#32ff7e",
-    "#7efff5",
-    ];
-
 ctx.lineWidth=2;
 
-function onClick(event) {
-    ctx.beginPath();
-    ctx.moveTo(event.offsetX,event.offsetY)
-    ctx.strokeStyle = colors[Math.floor(Math.random()*colors.length)];
+let isPainting = false;
+function onMove(event) {
+    if (isPainting) {
+        ctx.lineTo(event.offsetX,event.offsetY);
+        ctx.stroke();
+        return;
+    }
+    ctx.moveTo(event.offsetX,event.offsetY);
 }
 
-function onMouseMove(event) {
-    ctx.lineTo(event.offsetX,event.offsetY);
-    ctx.stroke();
+function startPainting(){
+    isPainting = true;
 }
-canvas.addEventListener("click", onClick);
-canvas.addEventListener("mousemove", onMouseMove);
+
+function cancelPainting(){
+    isPainting = false;
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
