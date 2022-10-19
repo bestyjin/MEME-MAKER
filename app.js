dfb6d33de0013157a52c445e78b05e8eeb45f94d
bestyjin <bestyjin0604@gmail.com>
2022-10-19 15:46:17 +0900

    Add Filling

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const modeBtn = document.getElementById("mode-btn");
 const colorOptions = Array.from(document.getElementsByClassName("color-option"));
 const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
@@ -9,6 +10,8 @@ canvas.height = 800;
 ctx.lineWidth=lineWidth.value;
 
 let isPainting = false;
+let isFilling = false;
+
 function onMove(event) {
     if (isPainting) {
         ctx.lineTo(event.offsetX,event.offsetY);
@@ -41,10 +44,27 @@ function onChangeColor(event){
     colorChange(event.target.value);
 }
 
+function onModeClick() {
+    if (isFilling){
+        isFilling = false;
+        modeBtn.innerText = "Fill";
+    } else {
+        isFilling = true;
+        modeBtn.innerText = "Draw";
+    }
+}
+
+function onCanvasClick() {
+    if (isFilling) {
+        ctx.fillRect(0,0,800,800);
+    }
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click", onCanvasClick);
 
 lineWidth.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onChangeColor);
@@ -55,3 +75,5 @@ function onColorClick(event) {
     color.value = colorValue;
 }
 colorOptions.forEach(color => color.addEventListener("click", onColorClick));
+
+modeBtn.addEventListener("click", onModeClick);
